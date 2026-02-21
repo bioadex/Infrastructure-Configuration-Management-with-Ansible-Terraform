5f78897d40baeaccb87607bf634f9ddd954a9908
aka-tech-it <bioadex@hotmail.com>
2026-02-22 00:05:23 +0100

    Add files via upload

---
+++ bootstrap/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "web_sg" {
+  name = "web-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c1fe732b5494dc14"  # Amazon Linux AMI
+  instance_type = var.instance_type
+  security_groups = [aws_security_group.web_sg.name]
+
+  tags = {
+    Name = "Terraform-Web"
+  }
+}
